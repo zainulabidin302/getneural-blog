1b87631b5e17f082a13703ba46600a6ad5da6d7e
Zain Ul Abidin <zainulabidin302@gmail.com>
2018-11-15 02:29:47 +0500

    updates

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../dist')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
